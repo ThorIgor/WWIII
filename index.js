58485edaba8230c4c8b6ed9132b76ef5fee9bd93
ThorIgor <igoeee0@gmail.com>
2021-04-22 19:03:26 +0300

    i hate this world

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static("www"));
 app.set("view engine", "ejs");
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/www/index.html'));
+    res.sendFile(path.join(__dirname + '/Frontend/www/index.html'));
 });
 
 app.listen(8080);
