3df998db7d358dc5f1b13add8b793a33b142447e
opadfnezig <opadfnezig@DESKTOP-HA40VCE>
2021-04-24 14:42:04 +0300

    added request, fixes

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ app.use(express.static("Frontend/www"));
 app.set("view engine", "ejs");
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'Frontend/www/index.html'));
+    res.sendFile(path.join(__dirname + '/Frontend/www/index.html'));
+});
+
+app.get("/testPage",function (req, res) {
+    res.sendFile(__dirname+'/Frontend/www/testPage.html');
+    res.sendFile(__dirname + '/Backend/data/TestData.json');
+    
 });
 
 app.listen(8080);
