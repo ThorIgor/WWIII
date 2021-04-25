e7604744a0188231d1ac92aa1dbd6915b458baeb
opadfnezig <opadfnezig@DESKTOP-HA40VCE>
2021-04-25 14:21:19 +0300

    grunt fix, now working keeeek

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,63 @@
+/**
+ * Created by Andriy on 10.03.2015.
+ */
 module.exports = function(grunt) {
-
-    // 1. Всё конфигурирование тут
-    grunt.initConfig({
+    //Налаштування збірки Grunt
+    var config = {
+        //Інформацію про проект з файлу package.json
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            dist: {
-                src: [
-                    'Frontend/src/*.js',
-                    'Frontend/src/test/*.js'
-                ],
-                dest: 'Frontend/www/assets/js/main.js',
+        //Конфігурація для модуля browserify (перетворює require(..) в код
+        browserify:     {
+            //Загальні налаштування (grunt-browserify)
+            options:      {
+
+                //brfs замість fs.readFileSync вставляє вміст файлу
+                transform:  [ require('brfs') ],
+                browserifyOptions: {
+                    //Папка з корнем джерельних кодів javascript
+                    basedir: "Frontend/src/js/"
+                }
+            },
+
+            //Збірка з назвою піца
+            pizza: {
+                src:        'Frontend/src/main.js',
+                dest:       'Frontend/www/assets/js/main.js'
             }
         }
+    };
 
-    });
+    //Налаштування відстежування змін в проекті
+    var watchDebug = {
+        options: {
+            'no-beep': true
+        },
+        //Назва завдання будь-яка
+        scripts: {
+            //На зміни в яких файлах реагувати
+            files: ['Frontend/src/**/*.js', 'Frontend/**/*.ejs'],
+            //Які завдання виконувати під час зміни в файлах
+            tasks: ['browserify:pizza']
+        }
+    };
 
-    // 3. Здесь мы сообщаем Grunt, что мы планируем использовать этот плагин:
-    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    // 4. Мы сообщаем Grunt, что нужно делать, когда мы введём "grunt" в терминале.
-    grunt.registerTask('default', ['concat']);
+    //Ініціалузвати Grunt
+    config.watch = watchDebug;
+    grunt.initConfig(config);
+
+    //Сказати які модулі необхідно виокристовувати
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    //Список завданнь по замовчування
+    grunt.registerTask('default',
+        [
+            'browserify:pizza',
+            //Інші завдання які необхідно виконати
+        ]
+    );
 
 };
