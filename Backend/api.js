cef8fddae11913f07b5e0ff99300488c6592bef4
opadfnezig <opadfnezig@DESKTOP-HA40VCE>
2021-04-25 15:36:49 +0300

    fixed backend(yes, again)

--- Backend/api.js
+++ Backend/api.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 var path = require('path');
-
+var testFolder = path.join(__dirname, '/data/');
 exports.sendTest= function(req, res) {
     res.json(req.params['testName']);
 };
 
+exports.sendTestList = function(req,res) {
+    
+}
 
-function getTest(name)
-{
-    try{return JSON.parse(fs.readFileSync(path.join(__dirname,'/data/' ,name, '.json')); }
+
+function getTest(name) {
+    try { return JSON.parse(fs.readFileSync(path.join(testFolder ,name, '.json'))); }
     catch{}
     return 0;
 }
-                          
-                          
+
+function getTestList() { 
+        return fs.readdirSync(testFolder);
+}
